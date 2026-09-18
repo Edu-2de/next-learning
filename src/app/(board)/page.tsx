5c20266dee06aecaf19ce6465aedd468da7c9c0c
Edu-2de <edupaim1712@gmail.com>
2026-09-18 16:15:10 -0300

    chore: move layout and page files to board folder

---
+++ src/app/(board)/page.tsx
@@ -0,0 +1,46 @@
+import {
+  ArchiveIcon,
+  MessageCircleIcon,
+  ThumbsDownIcon,
+  ThumbsUpIcon,
+} from "lucide-react";
+import { Section } from "@/components/section";
+import { Card } from "@/components/card";
+import { Button } from "@/components/button";
+
+export default function Home() {
+  return (
+    <main className="grid grid-cols-4 gap-5 flex-1 items-stretch">
+      <Section.Root>
+        <Section.Header>
+          <Section.Title>
+            <ArchiveIcon className="size-3" />
+            Backlog
+          </Section.Title>
+
+          <Section.IssueContent>16</Section.IssueContent>
+        </Section.Header>
+
+        <Section.Content>
+          <Card.Root>
+            <Card.Header>
+              <Card.Number>ECO-001</Card.Number>
+              <Card.Title>Implementar cartao de credito</Card.Title>
+            </Card.Header>
+            <Card.Footer>
+              <Button>
+                <ThumbsUpIcon className="size-3" />
+                <span className="text-sm">12</span>
+              </Button>
+
+              <Button>
+                <MessageCircleIcon className="size-3" />
+                <span className="text-sm">6</span>
+              </Button>
+            </Card.Footer>
+          </Card.Root>
+        </Section.Content>
+      </Section.Root>
+    </main>
+  );
+}
